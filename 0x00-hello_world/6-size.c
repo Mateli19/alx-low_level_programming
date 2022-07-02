b93daaf22dba26fbdb8567562917a3a3879862d5
Mateli19 <mateliboniface@gmail.com>
2022-07-01 23:46:44 -0700

    changes for task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
 
 /**
-* main - starting my program
-* int intTYPE - data type
+* main - Entry point
 *
-* Return 0-either-way
+* Return: Always 0 (success)
 */
 int main(void)
 {
-int intTYPE;
-float floatTYPE;
-char charTYPE;
-long int longTYPE;
-long long int longlongTYPE;
-/**
- * printf - prints value enclosed in a bracket appropriatly
- * parameter\n - Newline
- * parameter sizeof- Evaluate size of variable
- * @bytes(s) - character to be printed
- * @sizeof - evaluates size of variable
- */
-printf("size of a char: %zu byte(s)\n", sizeof(charTYPE));
-printf("size of an int: %zu byte(s)\n", sizeof(intTYPE))
-printf("size of an long int: %zu byte(s)\n", sizeof(longTYPE));
-printf("size of an long long int: %zu byte(s)\n", sizeof(longlongTYPE));
-printf("size of an float: %zu byte(s)\n" sizeof(floatTYPE));
-/* return - 0 */
-return (0);
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));                      return (0);
 }
