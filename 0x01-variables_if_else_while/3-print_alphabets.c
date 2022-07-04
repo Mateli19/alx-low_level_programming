19f6c4d264e26ad6c1f3b771283f1e0bfac3a89f
Mateli19 <mateliboniface@gmail.com>
2022-07-04 01:02:47 -0700

    changes for task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,15 +6,15 @@
  */
 int main(void);
 {
-int ch;
-for (ch = 97; ch <= 122; ch++)
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
 }
-for (ch = 65; ch <= 90; ch++)
+for (ch = 'A'; ch <= 'Z'; ch++)
 {
 putchar(ch);
 }
-putchar(10);
+putchar("\n");
 return (0);
 }
