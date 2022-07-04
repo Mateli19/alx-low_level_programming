801aa76df8fe9e8925f00cfa6892400e9a453daf
Mateli19 <mateliboniface@gmail.com>
2022-07-04 03:09:32 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+/**
+ * hexadecimal
+ * 0 1 2 3 4 5 6 7 8 9 a b c d e f
+ */
+int n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+char ch;
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
